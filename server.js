06db8c9277ceb74e5914fe694b3f19a01c40fe7f
Ian Kimble <ian.kimble@eRT.com>
2020-09-22 22:43:55 -0400

    frontend heroku

--- server.js
+++ server.js
@@ -157,11 +157,11 @@ app.listen(port, () => {
 });
 
 // React front end
-// app.use(express.static(path.join(__dirname, "client", "build")));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+app.use(express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// app.get("*", (request, response) => {
-//   response.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
